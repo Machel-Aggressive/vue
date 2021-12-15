fb1f003ec7dbf573f56e4d164e0455bafb68aff1
lizhixiang <276820745@qq.com>
2021-12-15 23:54:08 +0800

    add element-ui

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <el-row>
+    <el-button>默认按钮</el-button>
+    <el-button type="primary">主要按钮</el-button>
+    <el-button type="success">成功按钮</el-button>
+    <el-button type="info">信息按钮</el-button>
+    <el-button type="warning">警告按钮</el-button>
+    <el-button type="danger">危险按钮</el-button>
+  </el-row>
+  <el-date-picker
+    v-model="value1"
+    type="date"
+    placeholder="选择日期">
+  </el-date-picker>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+// import ElementUI from 'element-ui';
+// import 'element-ui/lib/theme-chalk/index.css';
+import { Button,Row,DatePicker } from 'element-ui'
 
-createApp(App).mount('#app')
+Vue.config.productionTip = false
+
+Vue.component(Button.name, Button)
+Vue.component(Row.name, Row)
+Vue.component(DatePicker.name, DatePicker)
+// Vue.use(ElementUI);
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
